f5e8839033be3643edc00c6ab0cf76332806b3c1
Eduardo Pereira <eduappereira@hotmail.com>
2024-08-23 11:24:59 -0300

    Itens added to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import {
     ButtonContainer,
     ButtonIcon,
     Container,
+    HeaderButtons,
     LogoContainer,
     SearchButton,
     SearchContainer,
@@ -13,33 +14,47 @@ import HamburguerIcon from '../../assets/hamburger.png';
 import Search from '../../assets/search.png'
 import Mic from '../../assets/microfone-gravador.png'
 import Logo from '../../assets/YouTube-Logo.png';
+import CameraIcon from '../../assets/video.png';
+import Bell from '../../assets/sino.png';
 
 function Header() {
     return (
-    <Container>
-        <LogoContainer>
-            <ButtonContainer>
-                <ButtonIcon alt="" src={HamburguerIcon}/>
-            </ButtonContainer>
-            <img
-                style={{ cursor: 'pointer', width: '100px' }}
-                alt=""
-                src={Logo}
-            />
-        </LogoContainer>
+        <Container>
+            <LogoContainer>
+                <ButtonContainer margin="0 10px 0 0">
+                    <ButtonIcon alt="" src={HamburguerIcon} />
+                </ButtonContainer>
+                <img
+                    style={{ cursor: 'pointer', width: '100px' }}
+                    alt=""
+                    src={Logo}
+                />
+            </LogoContainer>
 
-        <SearchContainer>
-            <SearchInputContainer>
-                <SearchInput placeholder="Pesquisar"/>
-            </SearchInputContainer>
-            <SearchButton>
-                <SearchIcon alt="" src={Search}/>
-            </SearchButton>
-            <ButtonContainer>
-                <ButtonIcon alt="" src={Mic}/>
-            </ButtonContainer>
-        </SearchContainer>
-    </Container>
+            <SearchContainer>
+                <SearchInputContainer>
+                    <SearchInput placeholder="Pesquisar" />
+                </SearchInputContainer>
+                <SearchButton>
+                    <SearchIcon alt="" src={Search} />
+                </SearchButton>
+                <ButtonContainer margin="0 0 0 10px" backgroundColor="#f0f0f0">
+                    <ButtonIcon alt="" src={Mic} />
+                </ButtonContainer>
+            </SearchContainer>
+
+            <HeaderButtons>
+                <ButtonContainer margin="0 0 0 10px">
+                    <ButtonIcon alt="" src={CameraIcon} />
+                </ButtonContainer>
+                <ButtonContainer margin="0 0 0 10px">
+                    <ButtonIcon alt="" src={Bell} />
+                </ButtonContainer>
+                <ButtonContainer margin="0 0 0 10px">E
+                </ButtonContainer>
+            </HeaderButtons>
+
+        </Container>
     )
 }
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.header`
     width: 100%;
-    height: 5%;
+    height: 56px;
     background-color: #fff;
     padding: 0% 16px;
     box-sizing: border-box;
@@ -17,20 +17,20 @@ export const LogoContainer = styled.div`
     align-items: center;
 `;
 
-export const ButtonContainer = styled.div`
+export const ButtonContainer = styled.div<{ margin?: string, backgroundColor?: string}>`
     width: 40px;
     height: 40px;
     border-radius: 45px;
-    
-    margin: 0 10px 0 0;
+    margin: ${({ margin }) => margin? margin : 0 };
+    background-color: ${({backgroundColor}) => backgroundColor? backgroundColor : '#0000' };
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
-    transition: 0.5s;
+    transition: 0.2s;
 
     &:hover {
-        background-color: #f2f2f2;
+        background-color: #bbbb;
     }
 `;
 
@@ -40,11 +40,14 @@ export const ButtonIcon = styled.img`
 
 export const SearchContainer = styled.div`
     display: flex;
+    height: 40px;
+    align-items: center;
 `;
 
 export const SearchInputContainer = styled.div`
+    display: flex;
     width: 30vh;
-    height: 35px;
+    height: 40px;
     border: 1px solid #d3d3d3;
     border-radius: 40px 0 0 40px;
     align-items: center;
@@ -60,8 +63,8 @@ export const SearchInput = styled.input`
 
 export const SearchButton = styled.div`
     width: 70px;
-    height: 35px;
     border-radius: 0 40px 40px 0;
+    height: 40px;
     border: 1px solid #d3d3d3;
     background-color: #f8f8f8;
     display: flex;
@@ -72,4 +75,9 @@ export const SearchButton = styled.div`
 
 export const SearchIcon = styled.img`
     width: 20px;
+`;
+
+export const HeaderButtons = styled.div`
+    display: flex;
+    justify-content: right;
 `;
